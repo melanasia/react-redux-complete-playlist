457f3df10cf18c936f085242c48eed8a926df516
melanasia <melanie.ali54@gmail.com>
2023-03-03 11:08:38 -0800

    progress needs fixing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,19 @@
 import Ninjas from './Ninjas';
 
 function App() {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'blue', id: 3 }
+    ]
+  }
   return (
     <div className="App">
       <h1>My React App</h1>
       <p>Welcome</p>
-      <Ninjas name='Ryu' age='25' belt='Black'/>
-      <Ninjas name='Yoshi' age='30' belt='Green'/>
+      {/* ninjas is referencing the array above and passing it down as a prop */}
+      <Ninjas ninjas={this.state.ninjas}/> 
     </div>
   );
 }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -4,17 +4,26 @@ import React, {Component} from 'react';
 
 class Ninjas extends Component{
     render(){
-        // console.log(this.props);
         // destructuring: we'll store the name of this.props in a variable called name
         // we'll store the age of this.props in a variable called age
         // we'll store the belt of this.props in a variable called belt
-        const {name, age, belt} = this.props;
+        const { ninjas } = this.props;
+        // next, we will cycle through this list of ninjas and output them to the DOM via the map method
+        // take this array, map it to a NEW array of HTML elements (one for each ninja)
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name}</div> 
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+             </div>  
+            )
+        });
         return(
-            <div className="ninja">
-               <div>Name: {name}</div> 
-               <div>Age: {age}</div>
-               <div>Belt: {belt}</div>
+            <div className = 'ninja-list'>
+                { ninjaList }
             </div>
+
         )
     }
 }
