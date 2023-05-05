126adfb3ec9f446d950d92720571f74cbc5c87a6
melanasia <melanie.ali54@gmail.com>
2023-05-05 08:19:52 -0700

    blog project updates

--- New-React-Tutorial/dojo-blog/src/Home.js
+++ New-React-Tutorial/dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -9,7 +10,8 @@ const Home = () => {
 
     return (
       <div className="home">
-         <BlogList blogs= { blogs }/>
+         <BlogList blogs={blogs} title="All Blogs!"/>
+         <BlogList blogs={blogs.filter((blog) => blog.author === 'Jane')} title="Jane's Blogs"/>
       </div>
     );
 }
