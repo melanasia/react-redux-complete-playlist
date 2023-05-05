cf2c811660263a25cec8cb6ce08b8eb0d6542c79
melanasia <melanie.ali54@gmail.com>
2023-05-05 11:22:16 -0700

    blog updates

--- New-React-Tutorial/dojo-blog/src/BlogList.js
+++ New-React-Tutorial/dojo-blog/src/BlogList.js
@@ -1,9 +1,9 @@
-const BlogList = (props) => {
+const BlogList = ({blogs, title}) => {
     
-    // access props on the object
-    const blogs = props.blogs;
-    const title = props.title;
-
+    // access props on the object:
+    // const blogs = props.blogs;
+    // const title = props.title;
+    // destructure the above (props) into ({blogs, title})
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
