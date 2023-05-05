07dac9f3b31fbafd84308ffaec9dea6a554a6e1e
melanasia <melanie.ali54@gmail.com>
2023-05-05 12:22:54 -0700

    updates

--- New-React-Tutorial/dojo-blog/src/BlogList.js
+++ New-React-Tutorial/dojo-blog/src/BlogList.js
@@ -1,9 +1,13 @@
-const BlogList = ({blogs, title}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
     
     // access props on the object:
     // const blogs = props.blogs;
     // const title = props.title;
     // destructure the above (props) into ({blogs, title})
+
+    // you don't put handleDelete here because you don't want to directly edit the blog's prop
+    // instead we use the setBlogs method within the Home component to update the state
+
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -11,6 +15,7 @@ const BlogList = ({blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>By { blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- New-React-Tutorial/dojo-blog/src/Home.js
+++ New-React-Tutorial/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,38 +8,26 @@ const Home = () => {
         {title: 'My skincare blog', body: "get anti-aging routines...", author: 'Jane', id: 3},
     ]);
 
+    const [name, setName] = useState('Michael');
+
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id)
+      setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+      console.log('useEffect ran')
+      // console.log(blogs);
+      console.log(name);
+    }, [name]);
+
     return (
       <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!"/>
-         <BlogList blogs={blogs.filter((blog) => blog.author === 'Jane')} title="Jane's Blogs"/>
+         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+         <button onClick = {() => setName('Fred')}>Change name</button>
+         <p>{name}</p>
       </div>
     );
 }
 
-// PREV (through tutorial #8, need to push to Github)
-// const Home = () => {
-//     // let name = 'mario';
-//     const [name, setName] = useState('mario');
-//     const [age, setAge] = useState(25);
-
-//     const handleClick = () => {
-//         setName('luigi');
-//         setAge(30);
-//     }
-
-//     // const handleClickAgain = (name) => {
-//     //     console.log('hello ' + name);
-
-//     // }
-
-//     return ( 
-//         <div className="class">
-//             <h2>Homepage</h2>
-//             <p>{ name } is { age } years old</p>
-//             <button onClick={handleClick}>Click me</button>
-//             {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
-//         </div>
-//      );
-// }
- 
 export default Home;
