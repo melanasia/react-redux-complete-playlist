252c3cfee4ed8284d9c42d3d58ae96e4fd603c37
melanasia <melanie.ali54@gmail.com>
2023-05-16 07:25:47 -0700

    made fetch component reusable

--- New-React-Tutorial/dojo-blog/src/Home.js
+++ New-React-Tutorial/dojo-blog/src/Home.js
@@ -1,28 +1,16 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
-    useEffect(() => {
-      setTimeout(() => {
-          fetch('http://localhost:8000/blogs')
-          .then(res => {
-          return res.json()
-          })
-          .then(data => {
-            setBlogs(data);
-            setIsLoading(false);
-          });
-          // console.log('useEffect ran')
-          // console.log(blogs);
-          // console.log(name);
-          }, 1000);
-    }, []);
+
+
 
     return (
       <div className="home">
+         { error && <div>{ error }</div>}
          {isLoading && <div>Loading...</div>}
          {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       </div>
