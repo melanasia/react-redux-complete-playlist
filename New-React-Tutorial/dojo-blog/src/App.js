e4221d64c6f62e4c36ba6f1f01e7f7811139dc25
melanasia <melanie.ali54@gmail.com>
2023-05-17 11:13:05 -0700

    adding routes

--- New-React-Tutorial/dojo-blog/src/App.js
+++ New-React-Tutorial/dojo-blog/src/App.js
@@ -1,18 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const title = 'Welcome to my blog';
-  const likes = 50;
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
