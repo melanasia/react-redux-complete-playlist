8bd37b5c3664f978e2c92a475bd676aba7c1d4a4
melanasia <melanie.ali54@gmail.com>
2023-05-19 10:21:56 -0700

    route params

--- New-React-Tutorial/dojo-blog/src/BlogList.js
+++ New-React-Tutorial/dojo-blog/src/BlogList.js
@@ -1,20 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({blogs, title}) => {
     
-    // access props on the object:
-    // const blogs = props.blogs;
-    // const title = props.title;
-    // destructure the above (props) into ({blogs, title})
-
-    // you don't put handleDelete here because you don't want to directly edit the blog's prop
-    // instead we use the setBlogs method within the Home component to update the state
-
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>By { blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>By { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
